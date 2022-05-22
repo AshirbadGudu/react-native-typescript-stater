a9fbee9082941b0d3a9b3ab12cea2bd11b50949a
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-05-22 07:51:34 +0530

    for react-navigation required screens created

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,1 +1,11 @@
-export {};
+// Auth Screens
+export {default as Login} from './auth/Login';
+export {default as Register} from './auth/Register';
+// Common Screens
+export {default as Splash} from './common/Splash';
+// Private Screens
+export {default as Home} from './private/Home';
+export {default as Profile} from './private/Profile';
+export {default as EditProfile} from './private/EditProfile';
+// Public Screens
+export {default as Onboarding} from './public/Onboarding';
