3c42271e7409a19f2a81dc80b5a16e604b9a32ab
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-06-23 07:27:31 +0530

    Chat details screen design complete

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -121,6 +121,22 @@ const ICONS = {
       />
     </Svg>
   ),
+  ChevronLeft: (props: IconType) => (
+    <Svg
+      stroke="currentColor"
+      fill="currentColor"
+      stroke-width="0"
+      viewBox="0 0 320 512"
+      {...props}
+      color={props.color || defaultProps.color}
+      width={props.size || defaultProps.size}
+      height={props.size || defaultProps.size}>
+      <Path
+        stroke={props.color || defaultProps.color}
+        d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"
+      />
+    </Svg>
+  ),
   ChevronRight: (props: IconType) => (
     <Svg
       stroke="currentColor"
---
+++ src/screens/private/chats/Details.tsx
@@ -0,0 +1,154 @@
+import {useNavigation} from '@react-navigation/native';
+import {ICONS} from 'assets';
+import {Avatar, Box, Column, Heading, Pressable, Row} from 'native-base';
+import React, {useCallback, useState} from 'react';
+import {Dimensions} from 'react-native';
+import {Bubble, Composer, GiftedChat, Send} from 'react-native-gifted-chat';
+import {StackAndTabType} from 'routes';
+import {COLORS} from 'styles';
+export const SCREEN_HEIGHT = Dimensions.get('screen').height;
+
+export default function Details() {
+  const navigation = useNavigation<StackAndTabType>();
+  const changeTypingStatus = useCallback(async txt => {}, []);
+  const [messages, setMessages] = useState([
+    {
+      _id: 1,
+      text: 'Hello Survi',
+      createdAt: new Date(),
+      user: {
+        _id: 'dd-02',
+        name: 'Anul Kesharwani',
+        avatar: 'https://randomuser.me/api/portraits/men/11.jpg',
+      },
+    },
+    {
+      _id: 2,
+      text: 'Please check',
+      createdAt: new Date(),
+      user: {
+        _id: 'dd-01',
+        avatar: 'https://randomuser.me/api/portraits/women/35.jpg',
+        name: 'Survi Jyoti',
+      },
+      pending: true,
+    },
+    {
+      _id: 3,
+      text: 'Today Class',
+      createdAt: new Date(),
+      user: {
+        _id: 'dd-01',
+        avatar: 'https://randomuser.me/api/portraits/women/35.jpg',
+        name: 'Survi Jyoti',
+      },
+      image:
+        'https://images.unsplash.com/photo-1542626991-cbc4e32524cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80',
+      // You can also add a video prop:
+      sent: true,
+      // Mark the message as received, using two tick
+      received: true,
+    },
+  ]);
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    );
+  }, []);
+  return (
+    <>
+      <Box
+        safeAreaTop
+        minH={'full'}
+        bg={{
+          linearGradient: {
+            colors: ['primary.50', 'primary.100'],
+            start: [1, 1, 0],
+            end: [1, 1, 0],
+          },
+        }}>
+        <>
+          <Row
+            alignItems="center"
+            space="4"
+            bg="primary.50"
+            p="2"
+            justifyContent={'space-between'}>
+            <Row alignItems="center" space="1.5">
+              <Pressable p="1" onPress={() => navigation.goBack()}>
+                <ICONS.ChevronLeft color={COLORS.primary[700]} />
+              </Pressable>
+              <Avatar
+                source={{uri: 'https://randomuser.me/api/portraits/men/11.jpg'}}
+                size="md">
+                AK
+                <Avatar.Badge bg="green.500" />
+              </Avatar>
+              <Column space={'0.5'}>
+                <Heading fontSize={'md'} color="primary.800">
+                  Anul Kesharwani
+                </Heading>
+                <Heading fontSize={'xs'} fontWeight="light">
+                  Online
+                </Heading>
+              </Column>
+            </Row>
+            <Row alignItems="center" space="2">
+              <Pressable
+                bg="primary.500"
+                p="2"
+                rounded={'md'}
+                onPress={() => navigation.goBack()}>
+                <ICONS.VideoCamera color="#fff" />
+              </Pressable>
+              <Pressable
+                bg="green.500"
+                p="2"
+                rounded={'md'}
+                onPress={() => navigation.goBack()}>
+                <ICONS.Phone color="#fff" />
+              </Pressable>
+            </Row>
+          </Row>
+          <GiftedChat
+            onSend={onSend}
+            user={{
+              _id: 'dd-01',
+              avatar: 'https://randomuser.me/api/portraits/women/35.jpg',
+              name: 'Survi Jyoti',
+            }}
+            scrollToBottom
+            showUserAvatar
+            // renderChatEmpty={renderChatEmpty}
+            // scrollToBottomComponent={scrollToBottomComponent}
+            // isTyping={true}
+            onInputTextChanged={txt => changeTypingStatus(txt)}
+            messages={messages}
+            renderComposer={props => (
+              <Composer
+                {...props}
+                textInputStyle={{fontFamily: 'Montserrat-Light'}}
+              />
+            )}
+            renderSend={props => (
+              <Send {...props} textStyle={{fontFamily: 'Montserrat-Medium'}} />
+            )}
+            renderBubble={props => (
+              <Bubble
+                {...props}
+                wrapperStyle={{
+                  left: {
+                    backgroundColor: COLORS.primary[50],
+                  },
+                  right: {
+                    backgroundColor: COLORS.primary[500],
+                  },
+                }}
+              />
+            )}
+          />
+        </>
+      </Box>
+    </>
+  );
+}
