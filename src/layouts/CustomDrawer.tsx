ef59c177afc7680eaf0d7e3754b9930245daeb97
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-06-23 07:55:11 +0530

    CustomDrawer UI modified, routing & logout added

--- src/layouts/CustomDrawer.tsx
+++ src/layouts/CustomDrawer.tsx
@@ -13,15 +13,19 @@ import {
 import {ICONS} from 'assets';
 import {useNavigation} from '@react-navigation/native';
 import {StackAndTabType} from 'routes';
+import {useAuth} from 'hooks';
+import {BackHandler, Linking} from 'react-native';
+import {COLORS} from 'styles';
 
 export default function CustomDrawer() {
   const navigation = useNavigation<StackAndTabType>();
+  const {setIsAuthenticated} = useAuth(state => state);
   const SIDE_MENUS = useMemo(
     () => [
       {
         id: '1',
         title: 'Home',
-        icon: <ICONS.Dashboard size={25} color={'gray'} />,
+        icon: <ICONS.Dashboard size={28} color={COLORS.primary[600]} />,
         onPress: () => {
           navigation.navigate('Home');
         },
@@ -29,7 +33,7 @@ export default function CustomDrawer() {
       {
         id: '2',
         title: 'Profile',
-        icon: <ICONS.User size={25} color={'gray'} />,
+        icon: <ICONS.User size={28} color={COLORS.primary[600]} />,
         onPress: () => {
           navigation.navigate('Profile');
         },
@@ -37,50 +41,50 @@ export default function CustomDrawer() {
       {
         id: '3',
         title: 'Notifications',
-        icon: <ICONS.Notification size={25} color={'gray'} />,
-        onPress: () => {},
+        icon: <ICONS.Notification size={28} color={COLORS.primary[600]} />,
+        onPress: () => navigation.navigate('Notifications'),
       },
       {
         id: '4',
         title: 'Live Chat',
-        icon: <ICONS.Chat size={25} color={'gray'} />,
-        onPress: () => {},
+        icon: <ICONS.Chat size={28} color={COLORS.primary[600]} />,
+        onPress: () => navigation.navigate('Chats'),
       },
       {
         id: '5',
         title: 'My Orders',
-        icon: <ICONS.Bag size={25} color={'gray'} />,
+        icon: <ICONS.Bag size={28} color={COLORS.primary[600]} />,
         onPress: () => {},
       },
       {
         id: '6',
         title: 'Customer Support',
-        icon: <ICONS.Help size={25} color={'gray'} />,
+        icon: <ICONS.Help size={28} color={COLORS.primary[600]} />,
         onPress: () => {},
       },
       {
         id: '7',
         title: 'About Us',
-        icon: <ICONS.Info size={25} color={'gray'} />,
+        icon: <ICONS.Info size={28} color={COLORS.primary[600]} />,
         onPress: () => {},
       },
       {
         id: '8',
         title: 'Rate Us',
-        icon: <ICONS.Star size={25} color={'gray'} />,
-        onPress: () => {},
+        icon: <ICONS.Star size={28} color={COLORS.primary[600]} />,
+        onPress: () => Linking.openURL('https://play.google.com/store/apps'),
       },
       {
         id: '9',
         title: 'Logout',
-        icon: <ICONS.Logout size={25} color={'gray'} />,
-        onPress: () => {},
+        icon: <ICONS.Logout size={28} color={COLORS.primary[600]} />,
+        onPress: () => setIsAuthenticated(false),
       },
       {
         id: '10',
         title: 'Close App',
-        icon: <ICONS.Close size={25} color={'gray'} />,
-        onPress: () => {},
+        icon: <ICONS.Close size={28} color={COLORS.primary[600]} />,
+        onPress: () => BackHandler.exitApp(),
       },
     ],
     [],
@@ -102,29 +106,36 @@ export default function CustomDrawer() {
                 }}
                 px="2"
                 py="5"
+                alignItems={'center'}
                 space={'3'}>
-                <Row w="1/6">
+                <>
                   <Avatar
-                    bg="primary.900"
-                    borderWidth={'1'}
-                    borderColor={'white'}>
-                    A
+                    borderColor={'#fff'}
+                    borderWidth="1"
+                    source={{
+                      uri: 'https://randomuser.me/api/portraits/men/46.jpg',
+                    }}
+                    rounded={'lg'}
+                    size="lg"
+                    _image={{rounded: 'lg'}}
+                    bg="primary.900">
+                    JD
                   </Avatar>
-                </Row>
-                <VStack w="80%">
+                </>
+                <VStack>
                   <Heading
                     color={'white'}
                     numberOfLines={1}
                     size={'md'}
                     ellipsizeMode="tail">
-                    Ashirbad Panigrahi
+                    Mr. John Doe
                   </Heading>
                   <Heading
                     color={'white'}
                     numberOfLines={1}
                     size={'xs'}
                     ellipsizeMode="tail">
-                    ashirbadpanigrahi@gmail.com
+                    johndoe@gmail.com
                   </Heading>
                 </VStack>
               </Row>
@@ -137,9 +148,11 @@ export default function CustomDrawer() {
                 onPress={() => {
                   item?.onPress();
                 }}>
-                <Row alignItems={'center'} px="2" py="4" space="2">
+                <Row alignItems={'center'} px="2" py="4" space="4">
                   {item.icon}
-                  <Text fontSize={18}>{item.title}</Text>
+                  <Heading fontWeight={'medium'} fontSize={'lg'}>
+                    {item.title}
+                  </Heading>
                 </Row>
                 <Divider />
               </Pressable>
