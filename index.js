23ce9465c9f4be9450166b69fef4afb13b9f5a76
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-07-13 18:26:47 +0530

    @react-navigation/drawer installed

--- index.js
+++ index.js
@@ -5,5 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+import 'react-native-gesture-handler'; // this line add for drawer navigation support
 AppRegistry.registerComponent(appName, () => App);
