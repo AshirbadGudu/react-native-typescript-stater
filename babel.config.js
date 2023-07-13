71eb92ca3f03dfb9c6719bf566512a510e637db1
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-07-13 18:41:59 +0530

    absolute import logic added

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,26 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'], // this line add for react-native-reanimated
+  plugins: [
+    'react-native-reanimated/plugin', // this line add for react-native-reanimated
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '~/assets': './src/assets',
+          '~/components': './src/components',
+          '~/components/core': './src/components/core',
+          '~/components/containers': './src/components/containers',
+          '~/components/shared': './src/components/shared',
+          '~/configs': './src/configs',
+          '~/constant': './src/constant',
+          '~/hooks': './src/hooks',
+          '~/routes': './src/routes',
+          '~/screens': './src/screens',
+          '~/styles': './src/styles',
+          '~/types': './src/types',
+        },
+      },
+    ],
+  ],
 };
