f4972a69ca21db239432d807fe8fac4b7ed7ee19
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-08-23 14:49:14 +0530

    Public Routes Completed

--- src/routes/public/index.tsx
+++ src/routes/public/index.tsx
@@ -1,6 +1,6 @@
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {Public} from '~/screens';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Auth, Public} from '~/screens';
 import {PublicRoutesTypes} from './types';
 
 const Stack = createNativeStackNavigator<PublicRoutesTypes>();
@@ -17,6 +17,9 @@ export default function PublicRoutes({
       initialRouteName={initialRouteName}
       screenOptions={{headerShown: false}}>
       <Stack.Screen name="Onboarding" component={Public.Onboarding} />
+      <Stack.Screen name="Login" component={Auth.Login} />
+      <Stack.Screen name="Register" component={Auth.Register} />
+      <Stack.Screen name="ForgotPassword" component={Auth.ForgotPassword} />
     </Stack.Navigator>
   );
 }
