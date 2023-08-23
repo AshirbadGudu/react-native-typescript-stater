5bc841965321a5f0a3a7e62f62edd78cbb0bd8d9
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-08-23 14:36:17 +0530

    Native Base Custom Theme Setup Complete

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import {NavigationContainer} from '@react-navigation/native';
 import {NativeBaseProvider} from 'native-base';
 import React from 'react';
 import {PublicRoutes} from '~/routes';
+import CustomTheme from '~/styles';
 
 export default function App(): JSX.Element {
   return (
     <NativeBaseProvider
-      // theme={CustomTheme}
+      theme={CustomTheme}
       config={{
         dependencies: {
           'linear-gradient': require('react-native-linear-gradient').default,
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,1 +1,23 @@
-export {};
+import {extendTheme} from 'native-base';
+
+export const COLORS = {
+  primary: {
+    50: '#D9E4FF',
+    100: '#B0C7FF',
+    200: '#87AAFF',
+    300: '#5E8DFF',
+    400: '#356FFF',
+    500: '#0C52FF',
+    600: '#0A43D6',
+    700: '#0835AD',
+    800: '#062682',
+    900: '#041856',
+    950: '#020A2B',
+  },
+};
+
+const CustomTheme = extendTheme({
+  colors: COLORS,
+});
+
+export default CustomTheme;
